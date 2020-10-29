ac8d32cefd41e0473da4efce33f348482a34a30a
Du Juan <jdu.xian@gmail.com>
2020-10-29 17:09:21 +0800

    feedback by Du Juan

--- src/Components/Group/Group.js
+++ src/Components/Group/Group.js
@@ -4,16 +4,19 @@ import DisplayTeam from './DisplayTeam';
 class Group extends Component {
   constructor() {
     super();
+    // TODO GTB-知识点: - teamList应该提取到App组件中，分组操作会引起这3个数据的变化
     this.state = {
       teamList: [],
     };
   }
 
   handleGroupingClick = () => {
+    // TODO GTB-知识点: - refreshGroupList为定义
     this.fetchGrouping().then(() => this.refreshGroupList());
   };
 
   fetchGrouping = () => {
+    // TODO GTB-工程实践: - 建议把数据请求提取到单独的service
     fetch('http://localhost:8080/grouping')
       .then((response) => response.json())
       .then((data) => {
@@ -25,12 +28,14 @@ class Group extends Component {
 
   render() {
     return (
+      // TODO GTB-知识点: * 没有使用语义标签
       <div>
         <h1>分组学员</h1>
         <button type="button" onClick={this.handleGroupingClick}>
           分组学员
         </button>
         {this.state.teamList.map((team) => (
+          // TODO GTB-知识点: - 直接传入team对象给DisplayTeam即可
           <DisplayTeam key={team.id} id={team.id} name={team.name} students={team.students} />
         ))}
       </div>
--- src/index.js
+++ src/index.js
@@ -3,4 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App/App';
 import './style/reboot.scss';
 
+// TODO GTB-完成度: - 和后端联调出错，api设计没有按照要求来
+// TODO GTB-工程实践: * 有小步提交，提交信息可以更表意一些
+// TODO GTB-知识点: * 有组件划分的意识
+// TODO GTB-工程实践: * 很多组件的命名不合理
 ReactDOM.render(<App />, document.getElementById('root'));
